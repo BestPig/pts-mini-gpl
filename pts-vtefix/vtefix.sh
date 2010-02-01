a7a5cd7956ba6c49f6964643920591ba442f6e60
ptspts <ptspts@5875f506-4f57-11de-a3de-f74ccfcb021d>
2010-02-01 08:33:23 +0000

    t

--- pts-vtefix/vtefix.sh
+++ pts-vtefix/vtefix.sh
@@ -37,6 +37,20 @@
 # $ objdump -d /usr/lib/libvte.so.9.2.17 | grep testb | grep '[$]0x4,'
 # 25cd8:       f6 80 4c 09 00 00 04    testb  $0x0,0x94c(%eax)
 # 25d0d:       f6 81 4c 09 00 00 04    testb  $0x4,0x94c(%ecx)
+#
+# !! i386 Ubuntu Karmic:
+# 2b7b0:       8b 80 84 09 00 00       mov    0x984(%eax),%eax
+# 2b7b6:       89 85 7c ff ff ff       mov    %eax,-0x84(%ebp)
+# ...
+# 2b9ed:	f6 85 7c ff ff ff 04 	testb  $0x4,-0x84(%ebp)
+# 2bad9:	f6 85 7c ff ff ff 04 	testb  $0x4,-0x84(%ebp)
+# 2bcbe:	f6 85 7c ff ff ff 04 	testb  $0x4,-0x84(%ebp)  # to fix
+# 2bcf5:	f6 85 7c ff ff ff 04 	testb  $0x4,-0x84(%ebp)
+# perl -e 'my$o=0x2bcbe;die unless open F, "+<", "/usr/lib/libvte.so.9"; die unless sysseek F, $o, 0; die unless 7 == sysread(F, $_, 7); print unpack("H*", $_)."\n"; die unless sysseek F, $o+6, 0; die unless 1 == syswrite(F, "\0")'
+# 2bcb8:       80 79 22 00             cmpb   $0x0,0x22(%ecx)
+# 2bcbc:       74 0d                   je     2bccb <vte_terminal_copy_primary
+# 2bcbe:       f6 85 7c ff ff ff 04    testb  $0x4,-0x84(%ebp)
+# 2bcc5:       0f 85 ba 00 00 00       jne    2bd85 <vte_terminal_copy_primary
 
 exec perl -we '
   use integer;
@@ -46,10 +60,14 @@ exec perl -we '
   die "error: file not found: $fn\n" if !open F, "<", $fn;
   $_ = join "", <F>;
   my @L;
-  while (/\xf6[\x80-\x8f].[\x09-\x0a]\0\0[\x00\x04]/sg) {
+  while (/\xf6[\x80-\x8f].[\x09-\x0a]\0\0[\x00\x04]
+         |\x80[\x78-\x7f].\0\x74.\xf6\x85.\xff\xff\xff[\x00\x04]
+         /sgx) {
     push @L, pos($_) - 1;
   }
-  if (@L == 2 and vec($_, $L[1], 8) == 4) {
+  pop @L if @L == 2 and vec($_, $L[0], 8) == 0xf6 and
+                          vec($_, $L[1], 8) == 0xf6;
+  if (@L == 1) {
     if (vec($_, $L[0], 8) == 4) {  # need to patch
       print "info: patching at offset $L[0]\n";
       die "error: cannot open file for writing: $fn\n" if !open F, "+<", $fn;
@@ -62,6 +80,6 @@ exec perl -we '
       exit 2;
     }
   } else {
-    die "error: instruction to patch not found\n";
+    die "error: instruction to patch not found (@L)\n";
   }
 ' -- "${1:-/usr/lib/libvte.so.9}"
